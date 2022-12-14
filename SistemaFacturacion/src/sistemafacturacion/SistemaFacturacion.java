469fed0b0f16935d343b5c18629ccff2b2e163b1
ItisYeWolf17 <105758299+ItisYeWolf17@users.noreply.github.com>
2022-12-14 16:08:57 -0600

    Sistema

---
+++ SistemaFacturacion/src/sistemafacturacion/SistemaFacturacion.java
@@ -0,0 +1,15 @@
+package sistemafacturacion;
+
+import Interfaces.InicioSesion;
+import javax.swing.JOptionPane;
+
+public class SistemaFacturacion {
+
+    public static void main(String[] args) {
+
+        InicioSesion log = new InicioSesion();
+        log.setVisible(true);
+
+    }
+
+}
